40acb60eb4c892b9a034aa81adc63ef58158a17a
Leotachy <lukas.andrijauskas@hotmail.com>
2024-03-16 15:16:28 +0200

    Use a different API URL

--- api/axios.js
+++ api/axios.js
@@ -1,11 +1,13 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://localhost:7046",
+  baseURL: "http://192.168.0.101:7046",
+  headers: { "Content-Type": "application/json",
+             "accept" : "*/*"},
 });
 
 export const axiosPrivate = axios.create({
-  baseURL: "https://localhost:7046",
-  headers: { "Content-Type": "application/json" },
-  withCredentials: true,
+  baseURL: "http://192.168.0.101:7046",
+  headers: { "Content-Type": "application/json",
+              "accept": "*/*"},
 });
--- api/user_api.js
+++ api/user_api.js
@@ -1,36 +1,23 @@
 import axios from "./axios";
 
 export const HandleLogin = (username, password) =>
+new Promise((resolve, reject) => {
+  axios
+    .post(`/Users/Login`, { username: username, password: password })
+    .then((x) => resolve(x.data))
+    .catch((x) => {
+      alert(x);
+      reject(x);
+    });
+});
+
+  export const HandleSignUp = (username, email, password) =>
   new Promise((resolve, reject) => {
     axios
-      .post(
-        `/session`,
-        { username: username, password: password },
-        {
-          headers: { "Content-Type": "application/json" },
-          withCredentials: true,
-        }
-      )
+      .post(`/Users/SignUp`, { username: username, email: email, password: password })
       .then((x) => resolve(x.data))
       .catch((x) => {
         alert(x);
         reject(x);
       });
   });
-
-export const HandleSignUp = (username, email, password) =>
-{
-
-
-  console.log(axios.getUri());
-
-  axios
-  .post('/Users/SignUp', { username: username, email: email, password: password })
-  .then(response => {console.log(response);})
-  .catch((x) => {
-
-    console.log(x);
-    console.log(x.data);
-    
-  });
-};
